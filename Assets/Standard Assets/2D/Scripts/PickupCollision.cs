4db905713aecf2d01149e832540322ce14264b6d
Alex Cherry <G00347106@gmit.ie>
2019-11-06 18:06:05 +0000

    Moving Repositories for cleaner cloning

---
+++ Assets/Standard Assets/2D/Scripts/PickupCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCollision : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("Protein"))
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
