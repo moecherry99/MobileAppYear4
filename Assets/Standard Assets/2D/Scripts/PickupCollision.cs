779ab366d49323efe41d4108e5e1238cfb7667d2
Alex Cherry <G00347106@gmit.ie>
2019-12-06 21:58:57 +0000

    added protein counter and text ui

--- Assets/Standard Assets/2D/Scripts/PickupCollision.cs
+++ Assets/Standard Assets/2D/Scripts/PickupCollision.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PickupCollision : MonoBehaviour
 {
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.name == "Player")
         {
+            ProteinScript.proteinValue += 1;
             Destroy(gameObject);
+             
         }
     }
+
+
 }
---
+++ Assets/Standard Assets/2D/Scripts/ProteinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProteinScript : MonoBehaviour
+{
+
+    public static int proteinValue;
+    Text protein;
+    // Start is called before the first frame update
+    void Start()
+    {
+        protein = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        protein.text = "Protein Shakes : " + proteinValue;
+    }
+}
