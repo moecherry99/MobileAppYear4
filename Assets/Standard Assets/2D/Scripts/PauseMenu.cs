690f95b6d3465a4e31282009c57e699004f9f409
Alex Cherry <G00347106@gmit.ie>
2019-12-09 22:37:03 +0000

    pause working

---
+++ Assets/Standard Assets/2D/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+            
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        // can also be used for slo-mo
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        // can also be used for slo-mo
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+}
